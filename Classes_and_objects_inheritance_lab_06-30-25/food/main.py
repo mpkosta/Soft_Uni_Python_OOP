5f15f975fccd0d409f389397a2a596c6923eae12
mpkosta <mpkosta@protonmail.com>
2025-06-30 19:30:59 +0300

    Inheritance

---
+++ Classes_and_objects_inheritance_lab_06-30-25/food/main.py
@@ -0,0 +1,5 @@
+from project.food import Food
+from project.fruit import Fruit
+
+fruit = Fruit("tomato", "2025-10-09")
+print(fruit.name)
---
+++ Classes_and_objects_inheritance_lab_06-30-25/multiple_inheritance/main.py
@@ -0,0 +1,5 @@
+from project.teacher import Teacher
+t = Teacher()
+print(t.get_fired())
+print(t.sleep())
+print(t.teach())
